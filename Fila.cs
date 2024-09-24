711d72cbf01dac4d5c17106ee08c37c4648b371d
MuriloRiber <you@example.com>
2024-09-24 20:01:35 -0300

    first-commit

---
+++ Fila.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace controlaPaciente
+{
+    internal class Fila
+    {
+        protected int Prioridade;
+        protected Paciente[] Vetor = new Paciente[11]; 
+
+
+    }
+}
